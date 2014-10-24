64ac1d8ae7ef51bee4473ba2f54aa21f4b11dabb
Hideto_Totoki <Hideto_Totoki@10.68.90.78>
2014-10-24 15:57:29 +0900

    ととき更新しました@ととき

--- HakodateMap/app/Controller/WebController.php
+++ HakodateMap/app/Controller/WebController.php
@@ -1,32 +1,90 @@
 <?php
-App::uses('AppController', 'Controller');
- 
+App::uses ( 'AppController', 'Controller' );
 class WebController extends AppController {
- 	public $name='Web';
- 	public $uses=null;
- 	public $layout="Web";
- 	
-    public function index() {
-        //$this -> autoLayout = false;
-        //$this -> autoRender = true;
-        //echo "え";
-    }
-    
-    public function home() {
-    	//$this -> autoLayout = false;
-    	//$this -> autoRender = true;
-    	//echo "え";
-    }
-    
-    public function about() {
-    	//$this -> autoLayout = false;
-    	//$this -> autoRender = true;
-    	//echo "え";
-    }
-    
-    public function courselist() {
-    	//$this -> autoLayout = false;
-    	//$this -> autoRender = true;
-    	//echo "え";
-    }
+	
+	// 検索結果のjson(デコード済み)を引数にそれを整理した配列を生成する
+	function parse($obj) {
+		$result = $keyList = $pushData = array ();
+		$i = 0;
+		
+		// key名を取得
+		foreach ( $obj ['head'] ['vars'] as $key ) {
+			$keyList += array (
+					$i => $key 
+			);
+			$i ++;
+		}
+		// 一件分ずつ検索結果を整理
+		foreach ( $obj ['results'] ['bindings'] as $val ) {
+			foreach ( $keyList as $keyName ) {
+				$pushData += array (
+						$keyName => $val [$keyName] ['value'] 
+				);
+			}
+			array_push ( $result, $pushData );
+			$pushData = array ();
+		}
+		return $result;
+	}
+	
+	// 引数のurlに対して問い合わせを実行
+	function runQuery($url) {
+		if (! function_exists ( 'curl_init' )) {
+			die ( 'CURL is not installed!' );
+		}
+		// Curlセッションを初期化
+		$ch = curl_init ();
+		// リクエストURLをセット
+		curl_setopt ( $ch, CURLOPT_URL, $url );
+		// 結果を文字列で取得
+		curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+				"Accept: application/json" 
+		) );
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+		// curlの実行
+		$response = curl_exec ( $ch );
+		curl_close ( $ch );
+		
+		return json_decode ( $response, true );
+	}
+	function courseNameAll() {
+		$query = '
+			PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?courseName WHERE {
+GRAPH <file:///var/lib/4store/machiaruki_akiba.rdf>{
+ ?s rdfs:label ?courseName;
+	}
+	}';
+		
+		$url = 'http://lod.per.c.fun.ac.jp:8000/sparql/?query=' . urlencode ( $query ) . '&output=json';
+		
+		return $url;
+	}
+	public $name = 'Web';
+	public $uses = null;
+	public $layout = "Web";
+	public function index() {
+		// $this -> autoLayout = false;
+		// $this -> autoRender = true;
+		// echo "え";
+	}
+	public function home() {
+		// $this -> autoLayout = false;
+		// $this -> autoRender = true;
+		// echo "え";
+	}
+	public function about() {
+		// $this -> autoLayout = false;
+		// $this -> autoRender = true;
+		// echo "え";
+	}
+	public function courselist(){
+		$obj=WebController::runQuery(WebController::courseNameAll());
+		$courseList=(WebController::parse($obj));
+		$this->set('courseList',$courseList);
+		// $this -> autoLayout = false;
+		// $this -> autoRender = true;
+		// echo "え";
+	}
 }
--- HakodateMap/app/View/Web/courselist.ctp
+++ HakodateMap/app/View/Web/courselist.ctp
@@ -12,6 +12,33 @@
 <!-- /gnav -->
 <!-- 以上は編集しないで -->
 
+<?php foreach($courseList as $course){
+			echo $course['courseName'];
+		}?>
+<?php for($i = 0; $i<10; $i++){
+	echo "<div id=\"gnav\">
+	<ul>
+	<li class=\"gnav01\"><a href=\"/cake/HakodateMapWeb/HakodateMap/Web/\">ホーム（仮）</a></li>
+	<li class=\"gnav02\"><a
+	href=\"/cake/HakodateMapWeb/HakodateMap/Web/about\">はこだてMap+について（仮）</a></li>
+	<li class=\"gnav03\"><a
+	href=\"/cake/HakodateMapWeb/HakodateMap/Web/courselist\"
+			class=\"selected\">コースリスト（仮）</a></li>
+			<li class=\"gnav04\"><a href=\"/cake/HakodateMapWeb/HakodateMap/Map\">マップ（仮）</a></li>
+			</ul>
+			</div>";}?>
+	<div id="gnav">
+	<ul>
+	<li class="gnav01"><a href="/cake/HakodateMapWeb/HakodateMap/Web/">ホーム（仮）</a></li>
+	<li class="gnav02"><a
+	href="/cake/HakodateMapWeb/HakodateMap/Web/about">はこだてMap+について（仮）</a></li>
+	<li class="gnav03"><a
+	href="/cake/HakodateMapWeb/HakodateMap/Web/courselist"
+			class="selected">コースリスト（仮）</a></li>
+			<li class="gnav04"><a href="/cake/HakodateMapWeb/HakodateMap/Map">マップ（仮）</a></li>
+			</ul>
+			</div>
+<?php ?>
 <html>
 <head>
 <titile>Index Page</titile>
